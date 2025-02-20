17f7fcc9f7313d300794eeba850cc2844c400181
AlexLC96 <soyunfurryowo@gmail.com>
2025-02-20 17:31:25 -0600

    Configuración inicial

---
+++ L01_2022CG650_2022CC601/Models/BlogContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+namespace L01_2022CG650_2022CC601.Models
+{
+    public class BlogContext : DbContext
+    {
+
+        public BlogContext(DbContextOptions<BlogContext> option) : base(option)
+
+        {
+
+        }
+    }
+}
--- L01_2022CG650_2022CC601/Program.cs
+++ L01_2022CG650_2022CC601/Program.cs
@@ -1,8 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using L01_2022CG650_2022CC601.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+//Inyeccion por dependencia de la base de datos
+builder.Services.AddDbContext<BlogContext>(options =>
+    options.UseSqlServer(
+             builder.Configuration.GetConnectionString("blogDbConnection")));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
